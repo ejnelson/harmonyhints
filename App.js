bd9f216be095aec1bac4d0de073512524dfd801f
Nelson, Erik-CW <Erik.Nelson2@bestbuy.com>
2017-10-30 09:59:08 -0500

    nav working

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import App from './app/index';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>
-          Changes you maasdfasdfasdfasdfasfake will automatically reload.
-        </Text>
-        <Text>Sasdfasdfasdfhake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+export default App;
